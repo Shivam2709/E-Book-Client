f7ff7f5482afc0989ed06ee146e5e7751f1cf7b5
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-17 15:56:29 +0530

    add book card

--- elib-client-app/src/app/(home)/page.tsx
+++ elib-client-app/src/app/(home)/page.tsx
@@ -1,7 +1,21 @@
 import Banner from "@/app/(home)/components/Banner";
+import BookList from "./components/BookList";
 
-export default function Home() {
+export default async function Home() {
+  // data fatching
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, {cache: 'no-cache'});
+
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the books");
+  }
+
+  const books = await response.json();
+  // console.log("books", books);
+
   return (
-    <Banner />
+    <>
+      <Banner />
+      <BookList books={books} />
+    </>
   );
 }
