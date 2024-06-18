ae9130657b54f25f9f2548923d40724851a29bab
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-18 12:37:42 +0530

    setUp initial project

---
+++ elib-dashboard/elib-dashboard/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+    <h2>HEllo world</h2>
+    <Button variant={'destructive'}>Click me</Button>
+    </>
+  )
+}
+
+export default App
