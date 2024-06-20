a96faff36ab8e0639483a53f4750d68af89bf38b
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-20 14:38:26 +0530

    add login and register page

--- ebook-dashboard/src/Pages/Register.tsx
+++ ebook-dashboard/src/Pages/Register.tsx
@@ -1,4 +1,3 @@
-// import Link from "next/link"
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,18 +10,24 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <section className="flex justify-center items-center h-screen">
-      <Card className="mx-auto max-w-sm">
+      <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your information to create an account
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
+            <div className="grid gap-2">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">Name</Label>
+                <Input id="name" placeholder="Max" required />
+              </div>
+            </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
@@ -33,23 +38,18 @@ const LoginPage = () => {
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link to={"/"} className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
-              </div>
-              <Input id="password" type="password" required />
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
             </div>
             <Button type="submit" className="w-full">
-              Login
+              Create an account
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link to={'/register'} className="underline">
-            Sign up
-          </Link>
+            Already have an account?{" "}
+            <Link to={'/login'} className="underline">
+              Login           
+            </Link>
           </div>
         </CardContent>
       </Card>
@@ -57,4 +57,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
