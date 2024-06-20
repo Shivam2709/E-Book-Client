aa0dc9d6a63c16db5e67c557f83c6e69c97029d7
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-20 15:46:21 +0530

    add dashboard layout

--- ebook-dashboard/src/router.tsx
+++ ebook-dashboard/src/router.tsx
@@ -2,11 +2,24 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/Pages/Login";
 import HomePage from "@/Pages/Home";
 import RegisterPage from "./Pages/Register";
+import DashboardLayout from "./Layouts/DashboardLayout";
+import BooksPage from "./Pages/Books";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <HomePage />
+        path: '/dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            }
+
+        ]
     },
     {
         path: "/login",
